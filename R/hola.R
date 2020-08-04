e1cf2f60f003ff4282f9ad77c78359518bebbf23
Leonardo Collado Torres <lcolladotor@gmail.com>
2020-08-04 15:28:28 -0400

    agregar la funcion hola()

---
+++ R/hola.R
@@ -0,0 +1,13 @@
+#' Greetings in Spanish
+#'
+#' This function says hi to people
+#'
+#' @param nombre The name of the person we are saying hi to in Spanish.
+#'
+#' @return A `character(1)` with the greetings in Spanish
+#' @export
+#'
+#' @examples
+hola <- function(nombre) {
+    paste0("¡Hola ", nombre, "!")
+}
